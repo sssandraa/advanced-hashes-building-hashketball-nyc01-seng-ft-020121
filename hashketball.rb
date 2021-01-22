6c4586a2d83bf853dae76c3b3790549e347f9ff9
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-22 23:13:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,14 +83,14 @@ def game_hash
       :slam_dunks => 10
     },
     {:player_name => "DeSagna Diop",
-      :number => ,
-      :shoe =>  ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks =>
+      :number => 2,
+      :shoe =>  14,
+      :points => 24,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
+      :slam_dunks => 5
     },
     {:player_name => "",
       :number => ,
