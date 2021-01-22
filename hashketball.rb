340bb5aae9193c4d93907933040b6476a578feef
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-22 23:12:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,14 +63,14 @@ def game_hash
   :colors => ["Turquoise", "Purple"],
   :players => [
     {:player_name => "Jeff Adrien",
-      :number => ,
-      :shoe =>  ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks =>
+      :number => 4,
+      :shoe =>  18,
+      :points => 10,
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2
     },
     {:player_name => "",
       :number => ,
