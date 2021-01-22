879bd324013cb12ee53b00708dd56943f794e57f
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-22 23:03:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,60 @@
 # Write your code here!
+# Write your code here!
 def game_hash
 {
 :home =>
   :team_name => "Brooklyn Nets",
   :colors => ["Black", "White"],
   :players => [
-    {
+    {:player_name => "",
+      :number => ,
+      :shoe =>  ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
+    },
+    {:player_name => "",
+      :number => ,
+      :shoe =>  ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
+    },
+    {:player_name => "",
+      :number => ,
+      :shoe =>  ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
+    },
+    {:player_name => "",
+      :number => ,
+      :shoe =>  ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
+    },
+    {:player_name => "",
+      :number => ,
+      :shoe =>  ,
+      :points => ,
+      :rebounds => ,
+      :assists => ,
+      :steals => ,
+      :blocks => ,
+      :slam_dunks =>
     }
   ]
 },
