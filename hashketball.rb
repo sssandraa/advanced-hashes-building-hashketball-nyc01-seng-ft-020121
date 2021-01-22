85fc25dc8d1f77d117a072212a96ff4d1bd0481b
Sandra Wazaz <sandrawazaz@gmail.com>
2021-01-22 23:02:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,20 @@
 # Write your code here!
+def game_hash
+{
+:home =>
+  :team_name => "Brooklyn Nets",
+  :colors => ["Black", "White"],
+  :players => [
+    {
+    }
+  ]
+},
+:away => {
+  :team_name => "Charlotte Hornets",
+  :colors => ["Turquoise", "Purple"],
+  :players => [{}]
+}
+}
+
+
+end
